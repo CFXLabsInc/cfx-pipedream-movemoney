560256cb1f8e6a79d6e9f5e0fc872fe53d3de091
yamen <yamen@cfxlabs.com>
2023-11-09 03:34:43 +0000

    Use new metadata identity

--- cko-webhooks-to-slack-p_o7CldRB/build_message/entry.js
+++ cko-webhooks-to-slack-p_o7CldRB/build_message/entry.js
@@ -1,12 +1,13 @@
 export default defineComponent({
   async run({ steps, $ }) {
+    const identityLink = (identityId) => `<https://interval.com/dashboard/cfxlabsinc/actions/identity_management/display?externalId=${identityId}|${identityId}>`;
     try {
     const { type, data } = steps.trigger.event.body;
     const messageType = type.split("_").join(" ");
     const { amount, response_summary } = data;
     const { card_category, card_type } = data.source;
-    const identity = data.customer?.email?.split("@")[0]
-    const identityString = identity ? ` by ${identity} ` : ""
+    const identity = data.metadata?.identity;
+    const identityString = identity ? ` by ${identityLink(identity)} ` : ""
 
     if (amount > 0) {
       return { message: `${messageType}${identityString} for $${(amount / 100).toFixed(2)} [${response_summary.toLowerCase()}] [${card_category.toLowerCase()} ${card_type.toLowerCase()}]`}
